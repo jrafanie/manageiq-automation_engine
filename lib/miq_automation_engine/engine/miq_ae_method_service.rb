0f5c2bf9c3e7ae84adc3409c5e9f5179ebfe7a64
Joe Rafaniello <jrafanie@redhat.com>
2020-03-20 16:30:20 -0400

    HORRIBLE HACK
    * Break out of this method early through return super for classes bypassing
    create_service_model_from_name, namely non-service models.
    * Build a list of non-service model classes within the MiaAeMethodService
    namespace and exclude these from create_service_model_from_name so they return
    super early.
    * It turns out that during loading of MiqAeServiceModelBase, other constants
    needed by it, also get loaded, such as MiqAeServiceObjectCommon and MiqAeServiceRbac,
    so we need to ensure that we check for MiqAeServiceModelBase.responds_to?
    create_service_model_from_name as the constant can be defined but it's methods
    NOT yet defined since these other constants get defined before
    MiaAeServiceModelBase completes loading.  Yikes!

--- lib/miq_automation_engine/engine/miq_ae_method_service.rb
+++ lib/miq_automation_engine/engine/miq_ae_method_service.rb
@@ -1,8 +1,28 @@
 module MiqAeMethodService
   def self.const_missing(name)
-    super unless defined?(MiqAeServiceModelBase)
+    return super if !defined?(MiqAeMethodService) && name == :MiqAeMethodService
 
-    MiqAeServiceModelBase.create_service_model_from_name(name) || super
+    horrible_list = [
+      :MiqAeServiceModelBase,
+      :MiqAeServiceModelLegacy,
+      :MiqAeServiceObject,
+      :MiqAeService,
+      :MiqAeServiceFront,
+      :MiqAeServiceVmdb,
+      :MiqAeServiceObjectCommon,
+      :MiqAeServiceRbac,
+      :MiqAeServiceConverter,
+      :MiqAeServiceMethods,
+      :Deprecation
+    ].each do |thing|
+      return super if name == thing && !MiqAeMethodService.const_defined?(thing)
+    end
+
+    if MiqAeServiceModelBase.respond_to?(:create_service_model_from_name)
+      MiqAeServiceModelBase.create_service_model_from_name(name)
+    else
+      super
+    end
   end
 end
 
